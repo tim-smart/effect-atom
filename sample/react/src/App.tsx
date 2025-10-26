8362b082582e7bca434e00688fb9066dee402c28
Marcin Bunsch <marcin.bunsch@gmail.com>
2025-10-26 10:49:47 +0100

    Add React sample

---
+++ sample/react/src/App.tsx
@@ -0,0 +1,32 @@
+import { Atom } from "@effect-atom/atom-react"
+import "./App.css"
+import reactLogo from "./assets/react.svg"
+import { HelloWorld } from "./components/HelloWorld"
+import viteLogo from "/vite.svg"
+
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+
+      <div className="card">
+        <HelloWorld />
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
